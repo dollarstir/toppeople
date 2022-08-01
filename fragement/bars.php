6738c296a3b14de5d4c45ff7bf85e997b85a29df
dollarstir <kpin463@gmail.com>
2022-08-01 17:20:58 +0000

    sdfsd

---
+++ fragement/bars.php
@@ -0,0 +1,35 @@
+<?php
+
+function begin($title)
+{
+    echo '<!DOCTYPE html>
+    <html lang="zxx">
+    
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    
+      <link rel="stylesheet" href="assets/css/meanmenu.css" />
+    
+      <link rel="stylesheet" href="assets/css/icofont.min.css" />
+    
+      <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+      <link rel="stylesheet" href="assets/css/owl.theme.default.min.css" />
+    
+      <link rel="stylesheet" href="assets/css/animate.min.css" />
+    
+      <link rel="stylesheet" href="assets/fonts/flaticon.css" />
+    
+      <link rel="stylesheet" href="assets/css/odometer.min.css" />
+    
+      <link rel="stylesheet" href="assets/css/style.css" />
+    
+      <link rel="stylesheet" href="assets/css/responsive.css" />
+    
+      <link rel="stylesheet" href="assets/css/theme-dark.css" />
+      <title>Topeople - '.$title.'</title>
+      <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+    </head>';
+}
