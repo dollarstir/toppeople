3968fb4ec126bc76bfa1a28267696e1a37831619
dollarstir <kpin463@gmail.com>
2022-08-01 17:26:54 +0000

    regrg

--- fragement/bars.php
+++ fragement/bars.php
@@ -16,27 +16,27 @@
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
-      <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="job/assets/css/bootstrap.min.css" />
     
-      <link rel="stylesheet" href="assets/css/meanmenu.css" />
+      <link rel="stylesheet" href="job/assets/css/meanmenu.css" />
     
-      <link rel="stylesheet" href="assets/css/icofont.min.css" />
+      <link rel="stylesheet" href="job/assets/css/icofont.min.css" />
     
-      <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-      <link rel="stylesheet" href="assets/css/owl.theme.default.min.css" />
+      <link rel="stylesheet" href="job/assets/css/owl.carousel.min.css" />
+      <link rel="stylesheet" href="job/assets/css/owl.theme.default.min.css" />
     
-      <link rel="stylesheet" href="assets/css/animate.min.css" />
+      <link rel="stylesheet" href="job/assets/css/animate.min.css" />
     
-      <link rel="stylesheet" href="assets/fonts/flaticon.css" />
+      <link rel="stylesheet" href="job/assets/fonts/flaticon.css" />
     
-      <link rel="stylesheet" href="assets/css/odometer.min.css" />
+      <link rel="stylesheet" href="job/assets/css/odometer.min.css" />
     
-      <link rel="stylesheet" href="assets/css/style.css" />
+      <link rel="stylesheet" href="job/assets/css/style.css" />
     
-      <link rel="stylesheet" href="assets/css/responsive.css" />
+      <link rel="stylesheet" href="job/assets/css/responsive.css" />
     
-      <link rel="stylesheet" href="assets/css/theme-dark.css" />
+      <link rel="stylesheet" href="job/assets/css/theme-dark.css" />
       <title>Topeople - '.$title.'</title>
-      <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+      <link rel="icon" type="image/png" href="job/assets/img/favicon.png" />
     </head>';
  }
