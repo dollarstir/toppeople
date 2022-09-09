5a80b60396dfd15a063d8d5fe98fd249f01ef384
Frederick Ennin <kpin463@gmail.com>
2022-09-09 11:26:53 +0000

    password reset employer 1

--- fragement/bars.php
+++ fragement/bars.php
@@ -801,7 +801,7 @@ function employerchangepass($oldpass, $newpass, $repass)
             echo 'Password does not match';
         } else {
             session_start();
-            if (authenticate('employers', [['id', '=', $_SESSION['employer']['id']], ['password', '=', md5($oldpass)]]) == 'success') {
+            if (authenticate('employers', [['id', '=', $_SESSION['employer']['id']], ['password', '=', md5($oldpass)]], 'AND') == 'success') {
                 if (update('employers', ['password' => md5($newpass)], ['id' => $_SESSION['employer']['id']]) == 'success') {
                     echo 'success';
                 } else {
