5a05e7882993617be38df52daffc841895c14d83
Frederick Ennin <kpin463@gmail.com>
2022-09-11 14:18:46 +0000

    sadsa

--- job/createjob.php
+++ job/createjob.php
@@ -1,33 +1,11 @@
-<!DOCTYPE html>
-<html lang="zxx">
+<?php
+involve('bars');
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+begin('Post Job');
 
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="assets/css/meanmenu.css">
-
-    <link rel="stylesheet" href="assets/css/icofont.min.css">
-
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-
-    <link rel="stylesheet" href="assets/css/animate.min.css">
-
-    <link rel="stylesheet" href="assets/fonts/flaticon.css">
-
-    <link rel="stylesheet" href="assets/css/odometer.min.css">
-
-    <link rel="stylesheet" href="assets/css/style.css">
-
-    <link rel="stylesheet" href="assets/css/responsive.css">
-
-    <link rel="stylesheet" href="assets/css/theme-dark.css">
-    <title>Topeople - Job Portal</title>
-    <link rel="icon" type="image/png" href="assets/img/favicon.png">
-</head>
+mainchecker('employer', 'employerauth');
+$employer = employer();
+?>
 
 <body>
 
@@ -46,7 +24,7 @@
     </div>
 
 
-    <?php include "sidebar.php" ?>
+    <?php employerbar(); ?>
 
 
     <div class="page-title-area">
@@ -101,7 +79,7 @@
                         <div class="col-lg-6">
                             <div class="form-group">
                                 <label>Company Name</label>
-                                <input type="text" class="form-control" placeholder="Winbrans.com">
+                                <input type="text" class="form-control" placeholder="Toppeople" value="<?php echo $employer['name']; ?>">
                             </div>
                         </div>
                         <div class="col-lg-6">
@@ -164,30 +142,10 @@
     </div>
 
 
-    <?php include "footer.php" ?>
-
-
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-
-    <script src="assets/js/jquery.meanmenu.js"></script>
-
-    <script src="assets/js/jquery.mixitup.min.js"></script>
-
-    <script src="assets/js/owl.carousel.min.js"></script>
-
-    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
-
-    <script src="assets/js/form-validator.min.js"></script>
-
-    <script src="assets/js/contact-form-script.js"></script>
-
-    <script src="assets/js/wow.min.js"></script>
-
-    <script src="assets/js/odometer.min.js"></script>
-    <script src="assets/js/jquery.appear.min.js"></script>
-
-    <script src="assets/js/custom.js"></script>
+    <?php
+        foot();
+        scripts();
+  ?>
 </body>
 
 </html>
